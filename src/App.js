81087eb9199c447486ae86c924404131c3c9222e
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-03-16 12:12:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [topHeadlines, setTopHeadlines] = useState([])
+
+  const getNews = ()=>{
+    console.log("clicked!")
+    // fetch("https://pokeapi.co/api/v2/pokemon")
+    //       .then(response => {
+    //         return response.json();
+    //     }).then(response => {
+    //         console.log(response);
+    //     }).catch(err=>{
+    //         console.log(err);
+    //     });
+    fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=lkjlkjkl")
+      .then(response => {
+        console.log("**********")
+        console.log(response)
+        console.log("**********")
+        return response.json()
+      })
+      .then(response =>{
+        console.log("!!!!!!!!!!!!")
+        console.log(response)
+        console.log("!!!!!!!!!!!!")
+        setTopHeadlines(response.articles)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick ={getNews} >Get some news</button>
+      {topHeadlines.map((newsArticle, idx)=>{
+        return <div class="card">
+        
+        <div class="card-body">
+          <h4 class="card-title">{newsArticle.title}</h4>
+          <p class="card-text">
+           {newsArticle.description}
+          </p>
+          <a href={newsArticle.url} class="btn btn-primary">Go to this new article</a>
+        </div>
+      </div>
+      
+      })}
     </div>
   );
 }
